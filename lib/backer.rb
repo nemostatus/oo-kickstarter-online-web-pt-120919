b035fdddb20d9a4ab5d00a9a90d92fcaf4c6683e
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-20 20:37:50 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,8 +1,7 @@
 class Backer 
   attr_reader :name 
-  def initialize(name,backed_projects)
+  def initialize(name)
     @name = name 
-    @backed_projects=backed_projects
     @backed_projects = []
   end 
 end
