bd980a6f7af734f08ddd4ce385069d3fb3a87ff7
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-20 20:53:36 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,7 +7,7 @@ class Backer
   end 
   def back_project(project)
    backed_projects << project
-   project.add_backer(self) unless project.backers
+   project.add_backer(self) unless project.backers.include?
   end 
   
   
